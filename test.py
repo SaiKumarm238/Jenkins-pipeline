c395926b7d2db0b377da04f7aa7a8eabdf40714a
Saikumar Manikonda <53387698+SaiKumarm238@users.noreply.github.com>
2023-06-28 19:05:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-def test_api():
-  assert True
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
